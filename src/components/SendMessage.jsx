79960c3097c6b8c1e6856a4fb33f4d93fb4da26d
hari <hariaum2003@gmail.com>
2023-04-17 21:50:21 +0530

    Small change in Send button

--- src/components/SendMessage.jsx
+++ src/components/SendMessage.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import { AiOutlineSend } from "react-icons/ai";
 
 const style = {
-  form: `h-14 w-full max-w-[100%] flex text-xl absolute bottom-0`,
+  form: `h-14 w-full max-w-[100%] flex rounded-lg text-xl absolute bottom-0`,
   input: `w-full text-xl p-3 bg-gray-900 text-white outline-none border-none`,
-  button: `w-[200px] rounded-xl bg-green-500`,
+  button: `flex items-center px-4 w-[125px] rounded-lg font-semibold bg-green-500`,
 };
 const SendMessage = () => {
   return (
     <form className={style.form}>
       <input className={style.input} type="text" placeholder="Message" />
       <button className={style.button} type="submit">
-        Send
+        Send &nbsp; {<AiOutlineSend />}
       </button>
     </form>
   );
